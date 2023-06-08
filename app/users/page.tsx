a8a925ed52b15151d0b24f4afdb725e10df83a4e
Franco Mitoire <franco.mitoire@gm2dev.com>
2023-06-08 12:07:43 -0300

    feat: call users

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,23 +1,22 @@
 import Link from "next/link";
+import { GET } from "@/app/api/users/route";
 // `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
 async function getData() {
-  const res = await fetch("https://api.publicapis.org/entries");
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  // Recommendation: handle errors
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
+  try {
+    const res = await GET();
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error("Failed to fetch data");
+    }
+    return res.json();
+  } catch (error) {
+    console.log(error);
   }
-
-  return res.json();
 }
 export default async function Page() {
   const data = await getData();
-
+  console.log("DATA ON PAGE", data);
   // make a list with first 100 users
-  const users = data.entries.slice(0, 10);
   return (
     <div>
       <h1>users</h1>
